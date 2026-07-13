f18ab31df12fa217b12af5587a37f320556d8c1d
Marcin Maziarz <stopaowcy@gmail.com>
2026-07-13 17:06:54 +0200

    dodaje strukture i style

--- strona/src/App.jsx
+++ strona/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
       <h1>STOPA</h1>
 <div className="socials">
   <a href="https://open.spotify.com/user/31o3vpjvf54nu3u7cbh4uftwslpa?si=d92c8c77521c4e9b" target="_blank">Spotify</a>
-  <a href="https://www.youtube.com/@stopa4203" target="_blank">YouTube</a>
-  <a href="https://steamcommunity.com/profiles/76561198880818625/" target="_blank">Steam</a>
-  <a href="https://www.instagram.com/marcin_maziarz433/" target="_blank">Instagram</a>
-  <a href="https://www.facebook.com/marcin.maziarz.545?locale=pl_PL" target="_blank">Instagram</a>
+  <a href="https://www.youtube.com/@stopa4203" target="_blank">   YouTube</a>
+  <a href="https://steamcommunity.com/profiles/76561198880818625/" target="_blank">   Steam</a>
+  <a href="https://www.instagram.com/marcin_maziarz433/" target="_blank">   Instagram</a>
+  <a href="https://www.facebook.com/marcin.maziarz.545?locale=pl_PL" target="_blank">   Instagram</a>
 </div>
     </div>
   );
