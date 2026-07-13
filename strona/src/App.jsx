2617f28e8c0b59d91261f8d4f07f260cbda178e7
Marcin Maziarz <stopaowcy@gmail.com>
2026-07-13 18:17:27 +0200

    dodaje strukture i style

--- strona/src/App.jsx
+++ strona/src/App.jsx
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="profile-card">
       <img src={mojAvatar} alt="Avatar" className="avatar" />
-      <h1>STOPAa</h1>
+      <h1>STOPA</h1>
 <div className="socials">
   <a href="https://open.spotify.com/user/31o3vpjvf54nu3u7cbh4uftwslpa?si=d92c8c77521c4e9b" target="_blank">Spotify</a>
   <a href="https://www.youtube.com/@stopa4203" target="_blank">   YouTube</a>
   <a href="https://steamcommunity.com/profiles/76561198880818625/" target="_blank">   Steam</a>
   <a href="https://www.instagram.com/marcin_maziarz433/" target="_blank">   Instagram</a>
-  <a href="https://www.facebook.com/marcin.maziarz.545?locale=pl_PL" target="_blank">   Instagram</a>
+  <a href="https://www.facebook.com/marcin.maziarz.545?locale=pl_PL" target="_blank">   Facebook</a>
 </div>
     </div>
   );
