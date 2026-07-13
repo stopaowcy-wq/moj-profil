1b0a980dfae053b8b3d8554cfef12247c3ec24c2
Marcin Maziarz <stopaowcy@gmail.com>
2026-07-13 17:06:07 +0200

    dodaje strukture i style

--- strona/src/App.jsx
+++ strona/src/App.jsx
@@ -4,14 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="profile-card">
-      <img src="https://via.placeholder.com/100" alt="Avatar" className="avatar" />
-      <h1>Navi</h1>
-      <p>If I were you, you would be mine</p>
-      <div className="socials">
-        <a href="https://spotify.com">Spotify</a> | 
-        <a href="https://twitch.tv"> Twitch</a> | 
-        <a href="https://steam.com"> Steam</a>
-      </div>
+      <img src="/baf4e793-29af-44d1-9e44-1d8c27f6295b/jpg" alt="Avatar" className="avatar" />
+      <h1>STOPA</h1>
+<div className="socials">
+  <a href="https://open.spotify.com/user/31o3vpjvf54nu3u7cbh4uftwslpa?si=d92c8c77521c4e9b" target="_blank">Spotify</a>
+  <a href="https://www.youtube.com/@stopa4203" target="_blank">YouTube</a>
+  <a href="https://steamcommunity.com/profiles/76561198880818625/" target="_blank">Steam</a>
+  <a href="https://www.instagram.com/marcin_maziarz433/" target="_blank">Instagram</a>
+  <a href="https://www.facebook.com/marcin.maziarz.545?locale=pl_PL" target="_blank">Instagram</a>
+</div>
     </div>
   );
 }
