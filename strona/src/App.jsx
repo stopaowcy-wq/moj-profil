c90b7ba0927dd030bd1280610a54100a3eed02a1
Marcin Maziarz <stopaowcy@gmail.com>
2026-07-13 16:02:56 +0200

    Pierwszy commit

---
+++ strona/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div style={{ backgroundColor: 'black', color: 'white', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>Strona w budowie</h1>
+    </div>
+  )
+}
+export default App
