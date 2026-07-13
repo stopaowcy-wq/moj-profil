e446e9be5362918c38d644ade3ccc5019c90c9b3
Marcin Maziarz <stopaowcy@gmail.com>
2026-07-13 19:02:15 +0200

    dodaje strukture i style

--- strona/src/App.jsx
+++ strona/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
 import mojAvatar from './assets/baf4e793-29af-44d1-9e44-1d8c27f6295b.jpg';
+import { FaSpotify, FaYoutube, FaSteam, FaInstagram, FaFacebook } from 'react-icons/fa';
 function App() {
   return (
     <div className="profile-card">
       <img src={mojAvatar} alt="Avatar" className="avatar" />
-      <h1>STOPA</h1>
+      <h1>Marcin</h1>
 <div className="socials">
-  <a href="https://open.spotify.com/user/31o3vpjvf54nu3u7cbh4uftwslpa?si=d92c8c77521c4e9b" target="_blank">Spotify</a>
-  <a href="https://www.youtube.com/@stopa4203" target="_blank">   YouTube</a>
-  <a href="https://steamcommunity.com/profiles/76561198880818625/" target="_blank">   Steam</a>
-  <a href="https://www.instagram.com/marcin_maziarz433/" target="_blank">   Instagram</a>
-  <a href="https://www.facebook.com/marcin.maziarz.545?locale=pl_PL" target="_blank">   Facebook</a>
+  <a href="TWÓJ_LINK_SPOTIFY" target="_blank"><FaSpotify /></a>
+  <a href="https://www.youtube.com/@stopa4203" target="_blank"><FaYoutube /></a>
+  <a href="https://steamcommunity.com/profiles/76561198880818625/" target="_blank"><FaSteam /></a>
+  <a href="https://www.instagram.com/marcin_maziarz433/" target="_blank"><FaInstagram /></a>
+  <a href="https://www.facebook.com/marcin.maziarz.545" target="_blank"><FaFacebook /></a>
 </div>
     </div>
   );
