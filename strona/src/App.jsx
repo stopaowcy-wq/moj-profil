a34cdd2e305047b9c9a26ed1b381ef60dd6c9b0c
Marcin Maziarz <stopaowcy@gmail.com>
2026-07-13 19:11:15 +0200

    dodaje strukture i style

--- strona/src/App.jsx
+++ strona/src/App.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
-import mojAvatar from './assets/baf4e793-29af-44d1-9e44-1d8c27f6295b.jpg';
 import { FaSpotify, FaYoutube, FaSteam, FaInstagram, FaFacebook } from 'react-icons/fa';
+import mojAvatar from './assets/baf4e793-29af-44d1-9e44-1d8c27f6295b.jpg';
+import videoBg from './assets/background.mp4';
+
 function App() {
   return (
-    <div className="profile-card">
-      <img src={mojAvatar} alt="Avatar" className="avatar" />
-      <h1>Marcin</h1>
-<div className="socials">
-  <a href="TWÓJ_LINK_SPOTIFY" target="_blank"><FaSpotify /></a>
-  <a href="https://www.youtube.com/@stopa4203" target="_blank"><FaYoutube /></a>
-  <a href="https://steamcommunity.com/profiles/76561198880818625/" target="_blank"><FaSteam /></a>
-  <a href="https://www.instagram.com/marcin_maziarz433/" target="_blank"><FaInstagram /></a>
-  <a href="https://www.facebook.com/marcin.maziarz.545" target="_blank"><FaFacebook /></a>
-</div>
+    <div className="main-wrapper">
+      <video autoPlay loop muted playsInline className="video-bg">
+        <source src={videoBg} type="video/mp4" />
+      </video>
+
+      <div className="profile-card">
+        <img src={mojAvatar} alt="Avatar" className="avatar" />
+        <h1>STOPA</h1>
+        <div className="socials">
+          <a href="https://open.spotify.com/user/31o3vpjvf54nu3u7cbh4uftwslpa" target="_blank"><FaSpotify /></a>
+          <a href="https://www.youtube.com/@stopa4203" target="_blank"><FaYoutube /></a>
+          <a href="https://steamcommunity.com/profiles/76561198880818625/" target="_blank"><FaSteam /></a>
+          <a href="https://www.instagram.com/marcin_maziarz433/" target="_blank"><FaInstagram /></a>
+          <a href="https://www.facebook.com/marcin.maziarz.545" target="_blank"><FaFacebook /></a>
+        </div>
+      </div>
     </div>
   );
 }
